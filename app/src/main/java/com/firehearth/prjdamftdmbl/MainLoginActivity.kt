c4820bb9584352cf3afe43cb429f1092e64cfea2
Ivan <iv4n9f@gmail.com>
2024-03-08 10:27:31 +0100

    0.02

--- app/src/main/java/com/firehearth/prjdamftdmbl/MainLoginActivity.kt
+++ app/src/main/java/com/firehearth/prjdamftdmbl/MainLoginActivity.kt
@@ -9,14 +9,20 @@ import android.widget.EditText
 
 class MainLoginActivity : AppCompatActivity() {
 
-    var emailfield: EditText = findViewById(R.id.MainLoginDisplayInputTextUser)
-    var passwordfield: EditText = findViewById(R.id.MainLoginDisplayInputTextPassword)
-
-    var loginButton: Button = findViewById(R.id.MainLoginDisplayLogin)
-    var registerButton: Button = findViewById(R.id.MainLoginDisplayRegister)
+    lateinit var emailfield: EditText
+    lateinit var passwordfield: EditText
+    lateinit var loginButton: Button
+    lateinit var registerButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_login)
+    }
+    override fun onStart(){
+        super.onStart()
+        emailfield = findViewById(R.id.MainLoginDisplayInputTextUser)
+        passwordfield = findViewById(R.id.MainLoginDisplayInputTextPassword)
+        loginButton = findViewById(R.id.MainLoginDisplayLogin)
+        registerButton = findViewById(R.id.MainLoginDisplayRegister)
         addListerners()
     }
     fun addListerners(){
