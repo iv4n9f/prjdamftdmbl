745b5aa60201325beb2276c9eccfd8704575958a
Ivan <iv4n9f@gmail.com>
2024-03-08 09:55:31 +0100

    0.02

--- app/src/main/java/com/firehearth/prjdamftdmbl/MainRegisterActivity.kt
+++ app/src/main/java/com/firehearth/prjdamftdmbl/MainRegisterActivity.kt
@@ -9,7 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main_register)
     }
 }
